1b66ffba14312086d7ee1c3f90f2cd941320dc18
Henry Veedahl <89225651+HenryNerd@users.noreply.github.com>
2026-07-17 00:02:55 -0500

    update edit UI

--- src/components/dispalayPurchase/purchaseCard.tsx
+++ src/components/dispalayPurchase/purchaseCard.tsx
@@ -25,7 +25,9 @@ import { useState } from "react"
 import Item from "../dispalayPurchase/itemCard"
 import Approver from "./approverCard"
 import { Button } from "../ui/button"
-import { Progress } from "@/components/ui/progress"
+import { Progress, ProgressLabel, ProgressValue } from "@/components/ui/progress"
+import { Input } from "@base-ui/react/input"
+import { Label } from "../ui/label"
 
 type request = {
     itemName: string;
@@ -58,6 +60,12 @@ export default function Purchase({ itemName, cost, requestor, catagory, requeste
     const [expiedited, setExpiedited] = useState(false);
     const [currentStatus, setStatus] = useState(status);
     const [editMode, setEditMode] = useState(false);
+
+    //track request info
+    const [name, setName] = useState(itemName);
+    const [itemCost, setItemCost] = useState(cost);
+    const [itemCatagory, setItemCaragory] = useState(catagory);
+
     const CategoryTitle = () => {
         switch (catagory) {
             case "Robot":
@@ -166,19 +174,23 @@ export default function Purchase({ itemName, cost, requestor, catagory, requeste
         );
     };
 
+    const calcPercent = () => {
+        return (Math.round(((4000 - calculatePrice()) / 4000) * 100))
+    }
+
     return (
         <div>
             <Card className="m-3 mt-4 p-2 bg-mist-700 h-fit cursor-pointer" onClick={() => setOpen(true)}>
                 <div className="flex gap-3 items-stretch">
                     <div>
                         <div className="flex">
-                            <CardTitle className="text-2xl font-bold text-zinc-100">{itemName}</CardTitle>
+                            <CardTitle className="text-2xl font-bold text-zinc-100">{name}</CardTitle>
                             {statusBadge()}
                             {(!expiedited && expieditedRequsted && userRole == "programDirector") && (
-                                <Badge className="text-sm ml-2 w-fit h-fit border-3 border-violet-500 bg-transparent font-bold text-violet-500">Expiedited Requested</Badge>
+                                <Badge className="text-sm ml-2 w-fit h-fit border-3 border-violet-500 bg-transparent font-bold text-violet-500">Expedited Requested</Badge>
                             )}
                             {(expiedited) && (
-                                <Badge className="text-sm ml-2 w-fit h-fit border-3 border-green-400 bg-transparent font-bold text-green-400">Expiedited</Badge>
+                                <Badge className="text-sm ml-2 w-fit h-fit border-3 border-green-400 bg-transparent font-bold text-green-400">Expedited</Badge>
                             )}
                         </div>
                         <CardDescription className="text-sm text-zinc-300 mt-1">Requested By: {requestor} on {requestedDate}</CardDescription>
@@ -197,26 +209,36 @@ export default function Purchase({ itemName, cost, requestor, catagory, requeste
                 <Drawer open={open} onOpenChange={setOpen} swipeDirection="right" modal={false}>
                     <DrawerContent className="[--drawer-inset:0px] rounded-tl-md rounded-tr-none border-0 w-1/4 bg-mist-600">
                         <Card className="p-0 mb-2 bg-mist-800 rounded-t-sm rounded-b-none rounded-tr-none gap-0">
-                            <div className="flex items-center justify-start mt-1 pb-0">
-                                <CardTitle className="ml-2 text-2xl p-2 font-jetbrains font-bold text-zinc-100">{itemName}</CardTitle>
-                                <div className="ml-atuo mr-2">
-                                    {statusBadge()}
+                            {(editMode) && (
+                                <div className="bg-amber-600 flex">
+                                    <h1 className="text-zinc-100 text-lg ml-2 mt-1">Edit Mode</h1>
+                                    <Button className="cursor-pointer ml-auto bg-zinc-100 text-black text-lg hover:bg-zinc-300 rounded-lg ml-auto text-sm mt-1 mb-1 mr-2" onClick={() => setEditMode(false)}>Save</Button>
                                 </div>
+                            )}
+                            <div className="mt-1 pb-0">
+                                {(editMode) && (
+                                    <Input type="url" placeholder="Request Title" value={name} onValueChange={(value) => setName(String(value))} className="bg-mist-700 rounded-md text-2xl ml-2 text-zinc-100 p-0 m-2 pl-2 font-bold"></Input>
+                                )}
+                                {(!editMode) && (
+                                    <div className="flex items-center justify-start mt-1 pb-0">
+                                        <CardTitle className="ml-2 text-2xl p-2 font-jetbrains font-bold text-zinc-100">{name}</CardTitle>
+                                        <div className="ml-atuo mr-2">
+                                            {statusBadge()}
+                                        </div>
+                                    </div>
+                                )}
                             </div>
-                            <CardDescription className="text-sm pl-4 text-zinc-100 mb-2">Requested By: {requestor}</CardDescription>
-                        </Card>
-                        <Card className="bg-mist-800 m-1 p-0 rounded-2xl gap-0">
-                            <div className="p-2 pt-0">
                             <div className="flex">
-                                <div>
-                                    <CardDescription className="text-sm pl-4 text-zinc-100 mb-2">Requested By: {requestor}</CardDescription>
-                                </div>
+                                <CardDescription className="text-sm pl-4 text-zinc-100 mb-2">Requested By: {requestor}</CardDescription>
                                 <div className="ml-auto">
                                     <DropdownMenu>
                                         <DropdownMenuTrigger render={<Button className="cursor-pointer bg-transparent hover:bg-transparent"><EllipsisVertical className="text-zinc-100 size-5 mb-1" /></Button>} />
                                         <DropdownMenuContent className="w-fit">
                                             <DropdownMenuGroup>
                                                 <DropdownMenuItem>Duplicate</DropdownMenuItem>
+                                                {(!editMode) && (
+                                                    <DropdownMenuItem onClick={() => setEditMode(true)}>Edit</DropdownMenuItem>
+                                                )}
                                                 {(!expieditedRequsted) && (<DropdownMenuItem onClick={() => setExpieditedRequsted(true)}>Request Expedite</DropdownMenuItem>)}
                                                 <DropdownMenuItem onClick={() => setStatus("rejected")}>Reject</DropdownMenuItem>
                                             </DropdownMenuGroup>
@@ -241,9 +263,14 @@ export default function Purchase({ itemName, cost, requestor, catagory, requeste
                                 </div>
                             </div>
                         </Card>
-                        <Card className="bg-mist-800 mt-2 m-1 p-0 rounded-2xl gap-0">
+                        <Card className="bg-mist-800 mt-2 m-1 m-1 p-0 rounded-2xl gap-0">
                             <div className="flex p-2 pb-0">
-                                <CardTitle className="text-lg ml-2 text-zinc-100 font-bold">Items</CardTitle>
+                                {(!editMode) && (
+                                    <CardTitle className="text-lg ml-2 text-zinc-100 font-bold">Items</CardTitle>
+                                )}
+                                {(editMode) && (
+                                    <CardTitle className="text-lg ml-2 text-zinc-100 font-bold">Edit Items</CardTitle>
+                                )}
                                 <div className="flex ml-auto">
                                 </div>
                             </div>
@@ -261,22 +288,31 @@ export default function Purchase({ itemName, cost, requestor, catagory, requeste
                                     />
                                 ))}
                             </div>
-                            <div className="p-2">
-                            <Progress
-                                className=""
-                                max={4000}
-                                value={calculatePrice()}
-                            />
-                            </div>
+                            {(editMode) && (
+                                <div className="p-2 bg-mist-700 m-2 rounded-md mt-3">
+                                    <Progress
+                                        className=""
+                                        max={4000}
+                                        value={calculatePrice()}
+                                    >
+                                        <div className="flex w-full">
+                                            <ProgressLabel className="text-zinc-100 text-sm mr-auto">Remaining Budget</ProgressLabel>
+                                            {(calcPercent() > 10) && (
+                                                <h1 className="ml-auto text-green-500 text-sm">{calcPercent()}%% Remains</h1>
+                                            )}
+                                            {(calcPercent() < 10 && !(calcPercent() < 0)) && (
+                                                <h1 className="ml-auto text-orange-500 text-sm">{calcPercent()}%% Remains</h1>
+                                            )}
+                                            {(calcPercent() < 0) && (
+                                                <h1 className="ml-auto text-red-500 text-sm">{calcPercent()}%% Remains</h1>
+                                            )}
+                                        </div>
+                                    </Progress>
+                                </div>
+                            )}
                             <div className="flex p-2">
                                 <h1 className="text-2xl text-zinc-100 font-bold">${calculatePrice().toFixed(2)}</h1>
                                 <div className="ml-auto">
-                                    {(!editMode) && (
-                                        <Button className="cursor-pointer ml-auto bg-zinc-100 text-black text-lg hover:bg-zinc-300 rounded-lg" onClick={() => setEditMode(true)}><Pencil /></Button>
-                                    )}
-                                    {(editMode) && (
-                                        <Button className="cursor-pointer ml-auto bg-zinc-100 text-black text-lg hover:bg-zinc-300 rounded-lg" onClick={() => setEditMode(false)}>Save</Button>
-                                    )}
                                 </div>
                             </div>
                         </Card>
